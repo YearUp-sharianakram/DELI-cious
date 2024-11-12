a5f6d7e8ff8f67ad50e279d001e69d41ff2be64d
sharianakram <sharianakram0503@gmail.com>
2024-11-12 10:46:11 -0500

    created outline, added userinterface

---
+++ src/main/java/com/pluralsight/Main.java
@@ -0,0 +1,9 @@
+package com.pluralsight;
+
+import com.pluralsight.Screens.HomeScreen;
+
+public class Main {
+    public static void main(String[] args) {
+        HomeScreen.homeScreen();
+    }
+}
---
+++ src/main/java/com/pluralsight/Screens/SandwichScreen.java
@@ -0,0 +1,29 @@
+package com.pluralsight.Screens;
+
+import com.pluralsight.util.Console;
+
+public class SandwichScreen {
+    public static void sandwichScreen(){
+        while (true) {
+            try {
+                System.out.println("\nWelcome to DELI-cious");
+                System.out.println("How may we help you today?");
+                System.out.println(" (N)- New Order");
+                System.out.println(" (X)- Exit");
+
+                String selection = Console.PromptForString();
+
+                if (selection.equalsIgnoreCase("N")) {
+                    System.out.println("go to sandwich screen");
+                } else if (selection.equalsIgnoreCase("X")) {
+                    System.out.println("Thank you for using the Accounting Ledger App! :D");
+                    return;
+                } else {
+                    System.out.println("Invalid Command");
+                }
+            } catch (Exception e) {
+                System.out.println("Invalid Command");
+            }
+        }
+    }
+}
